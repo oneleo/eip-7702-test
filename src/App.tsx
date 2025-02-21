673ddb5789b705af16c0f7e065ac37bf2a50b53f
陳敬翔 <irara.chen@token.im>
2025-02-21 19:06:10 +0800

    feat: Add EIP-7702 transaction button

--- src/App.tsx
+++ src/App.tsx
@@ -1,150 +1,484 @@
 import { useState, useEffect } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 
-import { JsonRpcProvider, Wallet } from "ethers";
+import {
+  HDNodeWallet,
+  AbstractProvider,
+  getDefaultProvider,
+  JsonRpcProvider,
+  AbiCoder,
+  ZeroAddress,
+  concat,
+  TransactionRequest,
+  Mnemonic,
+  getAddress,
+  dataLength,
+} from "ethers";
+
+import BatchCallDelegation from "../out/BatchCallDelegation.sol/BatchCallDelegation.json";
+import {
+  stringify,
+  delay,
+  getTransactionReceipt,
+  getExplorerUrl,
+} from "./util";
 
 import { createWalletClient, http, parseEther, encodeFunctionData } from "viem";
 import { privateKeyToAccount } from "viem/accounts";
-import { mekong } from "viem/chains";
+
 import {
   eip7702Actions,
-  type PrepareAuthorizationReturnType,
   type SignAuthorizationReturnType,
 } from "viem/experimental";
 
-const mekongProvider = new JsonRpcProvider(
-  import.meta.env.VITE_MEKONG_NODE_RPC_URL
-);
-const chainId = parseInt(import.meta.env.VITE_CHAIN_ID);
-const privateKey = import.meta.env.VITE_PRIVATE_KEY;
-const contractAddress = import.meta.env.VITE_CONTRACT_ADDRESS;
-const receiver = import.meta.env.VITE_RECEIVER_ADDRESS;
-
-const wallet = new Wallet(privateKey, mekongProvider);
-const walletClient = createWalletClient({
-  account: privateKeyToAccount(privateKey),
-  chain: mekong,
-  transport: http(),
-}).extend(eip7702Actions());
+import { mekong } from "viem/chains";
 
 function App() {
-  const [authorization, setAuthorization] =
-    useState<PrepareAuthorizationReturnType>({
-      contractAddress,
-      chainId,
-      nonce: 0,
-    });
+  const [nodeRpcUrl, setNodeRpcUrl] = useState<string>("");
+  const [provider, setProvider] = useState<AbstractProvider>(
+    getDefaultProvider()
+  );
+  const [chainId, setChainId] = useState<number>(0);
+
+  const [eoaToContract, setEoaToContract] = useState<HDNodeWallet>(
+    HDNodeWallet.createRandom()
+  );
+  const [contractDeployer, setContractDeployer] = useState<HDNodeWallet>(
+    HDNodeWallet.createRandom()
+  );
+  const [receiverCaller, setReceiverCaller] = useState<HDNodeWallet>(
+    HDNodeWallet.createRandom()
+  );
+
+  const [targetContractAddress, setTargetContractAddress] =
+    useState<string>(ZeroAddress);
+
   const [signedAuthorization, setSignedAuthorization] =
     useState<SignAuthorizationReturnType>({
-      contractAddress,
-      chainId,
+      contractAddress: `0x`,
+      chainId: 0,
       nonce: 0,
       r: "0x",
       s: "0x",
       yParity: 0,
     });
-  const [executing, setExecuting] = useState<boolean>(false);
+  const [transactionHash, setTransactionHash] = useState<string>(
+    "0x3142de7ba571e18173d69232d00d8609543d2e4335ba2688c2495dbf7b9e689a"
+  );
 
-  const prepareAuthorization = async () => {
-    setExecuting(true);
-    // console.log(`walletClient: ${stringify(walletClient)}`);
+  const [executing, setExecuting] = useState<string>(``);
+  const [message, setMessage] = useState<string>(``);
+  const [errorMessage, setErrorMessage] = useState<string>(``);
+
+  useEffect(() => {
+    const rpcUrl = import.meta.env.VITE_NODE_RPC_URL;
+    setNodeRpcUrl(rpcUrl);
+    const pvd = new JsonRpcProvider(rpcUrl);
+    setProvider(pvd);
+    setTargetContractAddress("0xe432A91273a4201BD355190C20B6C86f9492bfB8");
+
+    const mnemonic = Mnemonic.fromPhrase(import.meta.env.VITE_MNEMONIC);
+
+    setEoaToContract(
+      HDNodeWallet.fromMnemonic(mnemonic, `m/44'/60'/0'/0/0`).connect(pvd)
+    );
+    setContractDeployer(
+      HDNodeWallet.fromMnemonic(mnemonic, `m/44'/60'/0'/0/1`).connect(pvd)
+    );
+    setReceiverCaller(
+      HDNodeWallet.fromMnemonic(mnemonic, `m/44'/60'/0'/0/2`).connect(pvd)
+    );
+
+    const asyncFn = async () => {
+      setExecuting(`Getting chain ID...`);
+      const nwk = await pvd.getNetwork();
+      const cId = Number(nwk.chainId);
+      setChainId(cId);
+
+      const msg = `Chain ID: ${cId}`;
+      console.log(msg);
+      setMessage(msg);
+      setExecuting(``);
+    };
+
+    asyncFn();
+  }, []);
+
+  const deployTargetContract = async () => {
+    setExecuting(`Deploying new target contract...`);
+    const bytecode = BatchCallDelegation.bytecode.object;
+    const abiCoder = AbiCoder.defaultAbiCoder();
+    const constructorArgs = abiCoder.encode(
+      ["address"],
+      [contractDeployer.address]
+    );
+    const fullBytecode = concat([bytecode, constructorArgs]);
+    const tx: TransactionRequest = { data: fullBytecode };
 
     try {
-      const nonce = await wallet.getNonce();
-      console.log(`nonce: ${nonce}`);
+      const txResponse = await contractDeployer.sendTransaction(tx);
+
+      // Error: could not coalesce error (error={ "code": -32601, "message": "method ignored by upstream: 7 upstream skipped" }, payload={ "id": 14, "jsonrpc": "2.0", "method": "eth_getTransactionReceipt", "params": [ "0x963dfdc25bf556bbfb295b17a34d5946aa289bae40cb8e9f4d71e9630f47e35f" ] }, code=UNKNOWN_ERROR, version=6.13.5)
+      //   await txResponse.wait();
+      await delay(9000);
+
+      const txHash = txResponse.hash;
+
+      const msg1 = `Deployment TX: ${getExplorerUrl(chainId)}tx/${txHash}`;
+
+      console.log(msg1);
+      setMessage(msg1);
+
+      const txReceipt = await getTransactionReceipt(txHash);
+
+      if (txReceipt?.created_contract.hash) {
+        setTargetContractAddress(txReceipt?.created_contract.hash);
+      }
+
+      const msg2 = `Deployment TX Receipt: ${stringify(txReceipt)}`;
+
+      console.log(msg2);
+      setMessage(msg2);
+    } catch (error) {
+      const errorMessage =
+        error instanceof Error ? error.message : String(error);
+      console.error(`Error: ${errorMessage}`);
+    }
+
+    setExecuting(``);
+  };
+
+  const getCode = async () => {
+    setExecuting(`Getting code from EOA...`);
+    try {
+      const code = await provider.getCode(eoaToContract.address);
+      const msg = `code: ${code}`;
+      console.log(msg);
+      setMessage(msg);
+    } catch (error) {
+      const errorMessage =
+        error instanceof Error ? error.message : String(error);
+      console.error(`Error: ${errorMessage}`);
+    }
+    setExecuting(``);
+  };
+
+  const signEoaToContractTx = async () => {
+    setExecuting(`Signing transaction...`);
+
+    const walletClient = createWalletClient({
+      account: privateKeyToAccount(eoaToContract.privateKey as `0x${string}`),
+      transport: http(nodeRpcUrl),
+    }).extend(eip7702Actions());
+
+    try {
+      const nonce = await eoaToContract.getNonce();
+      console.log(`Nonce now is: ${nonce}`);
+
+      const viemEoaToContract = createWalletClient({
+        account: privateKeyToAccount(eoaToContract.privateKey as `0x${string}`),
+        transport: http(nodeRpcUrl),
+      }).extend(eip7702Actions());
 
       // Refer: https://viem.sh/experimental/eip7702
-      const auth = await walletClient.prepareAuthorization({
-        contractAddress,
+      const auth = await viemEoaToContract.prepareAuthorization({
+        contractAddress: targetContractAddress as `0x${string}`,
         chainId,
         nonce: nonce + 1,
       });
-      setAuthorization(auth);
-      console.log(`authorization: ${stringify(authorization)}`);
-    } catch (error) {
-      const errorMessage =
-        error instanceof Error ? error.message : String(error);
-      console.error(`Error: ${errorMessage}`);
-    }
-    setExecuting(false);
-  };
-
-  const signAuthorization = async () => {
-    setExecuting(true);
-    try {
-      // 1. Authorize injection of the Contract's bytecode into our Account.
-      const signedAuth = await walletClient.signAuthorization(authorization);
+      const signedAuth = await walletClient.signAuthorization(auth);
       setSignedAuthorization(signedAuth);
-      console.log(`signedAuthorization: ${stringify(signedAuth)}`);
+
+      const msg = `Signed TX: ${stringify(signedAuth)}`;
+      console.log(msg);
+      setMessage(msg);
     } catch (error) {
       const errorMessage =
         error instanceof Error ? error.message : String(error);
       console.error(`Error: ${errorMessage}`);
     }
-    setExecuting(false);
+    setExecuting(``);
   };
 
-  const sendBatchEther = async () => {
-    setExecuting(true);
+  const signContractToEoaTx = async () => {
+    setExecuting(`Signing transaction...`);
+    const viemEoaToContract = createWalletClient({
+      account: privateKeyToAccount(eoaToContract.privateKey as `0x${string}`),
+      transport: http(nodeRpcUrl),
+    }).extend(eip7702Actions());
+
+    try {
+      const nonce = await eoaToContract.getNonce();
+      console.log(`Nonce now is: ${nonce}`);
+
+      // Refer: https://viem.sh/experimental/eip7702
+      const auth = await viemEoaToContract.prepareAuthorization({
+        contractAddress: ZeroAddress as `0x${string}`,
+        chainId,
+        nonce: nonce + 1,
+      });
+      const signedAuth = await viemEoaToContract.signAuthorization(auth);
+      setSignedAuthorization(signedAuth);
+
+      const msg = `Signed TX: ${stringify(signedAuth)}`;
+      console.log(msg);
+      setMessage(msg);
+    } catch (error) {
+      const errorMessage =
+        error instanceof Error ? error.message : String(error);
+      console.error(`Error: ${errorMessage}`);
+    }
+    setExecuting(``);
+  };
+
+  const sendTransaction = async () => {
+    setExecuting(`Sending transaction...`);
+    const viemEoaToContract = createWalletClient({
+      account: privateKeyToAccount(eoaToContract.privateKey as `0x${string}`),
+      chain: mekong,
+      transport: http(nodeRpcUrl),
+    }).extend(eip7702Actions());
 
     try {
       // 2. Invoke the Contract's `execute` function to perform batch calls.
-      const hash = await walletClient.sendTransaction({
+      const txHash = await viemEoaToContract.sendTransaction({
+        authorizationList: [signedAuthorization],
+        to: ZeroAddress as `0x${string}`,
+      });
+      setTransactionHash(txHash);
+
+      const msg = `Sent TX: ${getExplorerUrl(chainId)}tx/${txHash}`;
+      console.log(msg);
+      setMessage(`${msg}`);
+    } catch (error) {
+      const errorMessage =
+        error instanceof Error ? error.message : String(error);
+      console.error(`Error: ${errorMessage}`);
+    }
+
+    setExecuting(``);
+  };
+
+  const sendTransactionWithContractCalldata = async () => {
+    setExecuting("Sending transaction with calldata...");
+    const viemEoaToContract = createWalletClient({
+      account: privateKeyToAccount(eoaToContract.privateKey as `0x${string}`),
+      chain: mekong,
+      transport: http(nodeRpcUrl),
+    }).extend(eip7702Actions());
+
+    try {
+      // 2. Invoke the Contract's `execute` function to perform batch calls.
+      const hash = await viemEoaToContract.sendTransaction({
         authorizationList: [signedAuthorization],
         data: encodeFunctionData({
-          abi,
+          abi: BatchCallDelegation.abi,
           functionName: "execute",
           args: [
             [
               {
                 data: "0x",
-                to: receiver,
+                to: receiverCaller.address as `0x${string}`,
                 value: parseEther("0.001"),
               },
               {
                 data: "0x",
-                to: receiver,
+                to: receiverCaller.address as `0x${string}`,
                 value: parseEther("0.002"),
               },
             ],
           ],
         }),
-        to: walletClient.account.address,
+        to: viemEoaToContract.account.address,
       });
-      console.log(`hash: https://explorer.mekong.ethpandaops.io/tx/${hash}`);
+      const msg = `Sent TX: ${getExplorerUrl(chainId)}tx/${hash}`;
+      console.log(msg);
+      setMessage(msg);
     } catch (error) {
       const errorMessage =
         error instanceof Error ? error.message : String(error);
       console.error(`Error: ${errorMessage}`);
     }
 
-    setExecuting(false);
+    setExecuting(``);
+  };
+
+  const handleTargetContractChange = (input: string) => {
+    setTargetContractAddress(input);
+
+    try {
+      getAddress(input);
+      setErrorMessage(``);
+    } catch (error) {
+      setErrorMessage(`Invalid address`);
+    }
+  };
+
+  const handleTransactionHashChange = (input: string) => {
+    setTransactionHash(input);
+    try {
+      dataLength(input) != 32;
+    } catch (error) {
+      setErrorMessage(`Invalid transaction hash`);
+    }
+  };
+
+  const getTransaction = async () => {
+    try {
+      const txReceipt = await getTransactionReceipt(transactionHash);
+
+      const msg = `Transaction Receipt: ${stringify(txReceipt)}`;
+      console.log(msg);
+      setMessage(msg);
+      return txReceipt;
+    } catch (error) {
+      console.error("Error fetching transaction:", error);
+    }
   };
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
       <h1>Vite + React</h1>
+
       <div className="card">
-        <button onClick={prepareAuthorization} disabled={executing}>
-          Prepare Authorization
+        <div>
+          <label>
+            EIP-7702 EOA/Contract:{" "}
+            <a
+              href={`${getExplorerUrl(chainId)}address/${
+                eoaToContract.address
+              }`}
+              target="_blank"
+            >
+              {`${eoaToContract.address}`}
+            </a>
+          </label>
+        </div>
+
+        <div>
+          <label>
+            Target Contract:{" "}
+            <input
+              type="text"
+              style={{ width: "350px" }}
+              value={targetContractAddress}
+              onChange={(e) => handleTargetContractChange(e.target.value)}
+              placeholder="Enter target contract address"
+            />
+          </label>
+        </div>
+        <div>
+          <a
+            href={`${getExplorerUrl(chainId)}address/${targetContractAddress}`}
+            target="_blank"
+            style={{
+              visibility: errorMessage ? "hidden" : "visible",
+            }}
+          >
+            {`${getExplorerUrl(chainId)}address/${targetContractAddress}`}
+          </a>
+        </div>
+
+        <div>
+          {executing && (
+            <div>
+              <span>Status: </span>
+              <span style={{ color: "white" }}>{executing}</span>
+            </div>
+          )}
+          {errorMessage && (
+            <div>
+              <span>Error: </span>
+              <span
+                style={{
+                  color: "red",
+                  textAlign: "left",
+                }}
+              >
+                {errorMessage}
+              </span>
+            </div>
+          )}
+        </div>
+      </div>
+
+      {/* <div className="card">
+        <button onClick={deployTargetContract} disabled={!!executing || !!errorMessage}>
+          Deploy New Target Contract
         </button>
-        <button onClick={signAuthorization} disabled={executing}>
-          Sign Authorization
-        </button>
-        <button onClick={sendBatchEther} disabled={executing}>
-          Send Batch Ether
+      </div> */}
+
+      <div className="card">
+        <button onClick={getCode} disabled={!!executing || !!errorMessage}>
+          Get EIP-7702 EOA/Contract Code
         </button>
       </div>
+
+      <div className="card">
+        <button
+          onClick={signEoaToContractTx}
+          disabled={!!executing || !!errorMessage}
+        >
+          Sign EOA to Contract TX
+        </button>
+        <button
+          onClick={signContractToEoaTx}
+          disabled={!!executing || !!errorMessage}
+        >
+          Sign Contract to EOA TX
+        </button>
+      </div>
+
+      <div className="card">
+        <button
+          onClick={sendTransaction}
+          disabled={!!executing || !!errorMessage}
+        >
+          Send TX
+        </button>
+        <button
+          onClick={sendTransactionWithContractCalldata}
+          disabled={!!executing || !!errorMessage}
+        >
+          Send TX with Contract Calldata
+        </button>
+      </div>
+
+      <div>
+        <label>
+          Transaction Hash:{" "}
+          <input
+            type="text"
+            style={{ width: "500px" }}
+            value={transactionHash}
+            onChange={(e) => handleTransactionHashChange(e.target.value)}
+            placeholder="Enter recipient address"
+          />
+        </label>
+      </div>
+      <div>
+        <button
+          onClick={getTransaction}
+          disabled={!!executing || !!errorMessage}
+        >
+          Get Transaction Receipt
+        </button>
+      </div>
+
+      <div className="card">
+        Message:{" "}
+        {message && (
+          <pre
+            style={{
+              color: "white",
+              textAlign: "left",
+            }}
+          >
+            {message}
+          </pre>
+        )}
+      </div>
+
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
@@ -153,41 +487,3 @@ function App() {
 }
 
 export default App;
-
-const abi = [
-  {
-    type: "function",
-    name: "execute",
-    inputs: [
-      {
-        name: "calls",
-        type: "tuple[]",
-        components: [
-          {
-            name: "data",
-            type: "bytes",
-          },
-          {
-            name: "to",
-            type: "address",
-          },
-          {
-            name: "value",
-            type: "uint256",
-          },
-        ],
-      },
-    ],
-    outputs: [],
-    stateMutability: "payable",
-  },
-] as const;
-
-const stringify = (info: any) =>
-  JSON.stringify(
-    info,
-    (_, value) => {
-      return typeof value === "bigint" ? value.toString() : value;
-    },
-    2
-  );
---
+++ src/util.ts
@@ -0,0 +1,178 @@
+export type Fee = {
+  type: "maximum" | "actual";
+  value: string;
+};
+
+export type AddressParam = {
+  hash: string;
+  implementation_name: string;
+  name: string;
+  ens_domain_name?: string;
+  metadata: Record<string, any>;
+  is_contract: boolean;
+  private_tags?: AddressTag[];
+  watchlist_names?: WatchlistName[];
+  public_tags?: AddressTag[];
+  is_verified: boolean;
+};
+
+export type AddressTag = {
+  address_hash: string;
+  display_name: string;
+  label: string;
+};
+
+export type WatchlistName = {
+  display_name: string;
+  label: string;
+};
+
+export type TokenTransfer = {
+  // Define the TokenTransfer fields as needed
+};
+
+export type TransactionAction =
+  | TransactionActionAaveV3LiquidationCall
+  | TransactionActionAaveV3BSWRF
+  | TransactionActionAaveV3EnableDisableCollateral
+  | TransactionActionUniswapV3MintNFT
+  | TransactionActionUniswapV3BCS;
+
+export type TransactionActionAaveV3LiquidationCall = {
+  data: Record<string, any>;
+  protocol: "aave_v3";
+  type: "liquidation_call";
+};
+
+export type TransactionActionAaveV3BSWRF = {
+  data: Record<string, any>;
+  protocol: "aave_v3";
+  type: "borrow" | "supply" | "withdraw" | "repay" | "flash_loan";
+};
+
+export type TransactionActionAaveV3EnableDisableCollateral = {
+  data: Record<string, any>;
+  protocol: "aave_v3";
+  type: "enable_collateral" | "disable_collateral";
+};
+
+export type TransactionActionUniswapV3MintNFT = {
+  data: Record<string, any>;
+  protocol: "uniswap_v3";
+  type: "mint_nft";
+};
+
+export type TransactionActionUniswapV3BCS = {
+  data: Record<string, any>;
+  protocol: "uniswap_v3";
+  type: "burn" | "collect" | "swap";
+};
+
+export type DecodedInputParameter = {
+  name: string;
+  type: string;
+  value: string;
+};
+
+export type DecodedInput = {
+  method_call: string;
+  method_id: string;
+  parameters: DecodedInputParameter[];
+};
+
+export type Transaction = {
+  timestamp: string;
+  fee: Fee;
+  gas_limit: number;
+  block_number: number;
+  status: "ok" | "error";
+  method: string;
+  confirmations: number;
+  type: number;
+  exchange_rate: string;
+  to: AddressParam;
+  transaction_burnt_fee: string;
+  max_fee_per_gas: string;
+  result: string;
+  hash: string;
+  gas_price: string;
+  priority_fee: string;
+  base_fee_per_gas: string;
+  from: AddressParam;
+  token_transfers: TokenTransfer[];
+  transaction_types: string[];
+  gas_used: string;
+  created_contract: AddressParam;
+  position: number;
+  nonce: number;
+  has_error_in_internal_transactions: boolean;
+  actions: TransactionAction[];
+  decoded_input: DecodedInput;
+  token_transfers_overflow: boolean;
+  raw_input: string;
+  value: string;
+  max_priority_fee_per_gas: string;
+  revert_reason: string;
+  confirmation_duration: number[];
+  transaction_tag: string;
+};
+
+export const stringify = (info: any) =>
+  JSON.stringify(
+    info,
+    (_, value) => {
+      return typeof value === "bigint" ? value.toString() : value;
+    },
+    2
+  );
+
+export const delay = (ms: number): Promise<void> => {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+};
+
+export const getExplorerUrl = (chainId: number): string => {
+  switch (chainId) {
+    case 7078815900: {
+      return `https://explorer.mekong.ethpandaops.io/`;
+    }
+    case 17000: {
+      return `https://holesky.etherscan.io/`;
+    }
+    default: {
+      return `https://explorer.mekong.ethpandaops.io/`;
+    }
+  }
+};
+
+export const getTransactionReceipt = async (
+  hash: string
+): Promise<Transaction | null> => {
+  const fetchUrl = `https://explorer-api.mekong.ethpandaops.io/api/v2/transactions/${hash}`;
+
+  try {
+    const response = await fetch(fetchUrl, {
+      method: "GET",
+      headers: { Accept: "application/json" },
+    });
+
+    if (!response.ok) {
+      throw new Error(
+        `Network error: ${response.status} ${response.statusText}`
+      );
+    }
+
+    const data = await response.json();
+    if (!data) {
+      return null;
+    }
+
+    return data as Transaction;
+  } catch (error) {
+    console.error(
+      `Error fetching transaction receipt: ${
+        error instanceof Error ? error.message : String(error)
+      }`
+    );
+    throw error;
+  }
+};
