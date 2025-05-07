6b58b0ca1c20e1c0d53586f03a3619d6e6d8e5a9
irara.chen@token.im <irara.chen@token.im>
2025-05-07 11:51:22 +0800

    chore: Try to fix the PREP r value of signature

--- src/util/general.ts
+++ src/util/general.ts
@@ -168,6 +168,7 @@ export const generateSignature = (
 
     // Random r (32 bytes)
     const r = hexlify(randomBytes(32));
+    // const r = hexlify(`0x${"e".repeat(64)}`);
 
     // v is usually 0x1b or 0x1c (27 or 28)
     const v = Math.random() > 0.5 ? "0x1b" : "0x1c";
